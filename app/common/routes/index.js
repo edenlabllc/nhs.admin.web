76b0e3b55467857bbb74dc4899fe2389fae35a4c
Alexey Bondarenko <alexeybondarenko@me.com>
2017-04-19 10:52:12 +0300

    Init project

---
+++ app/common/routes/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+import App from 'containers/layouts/App';
+import Main from 'containers/layouts/Main';
+
+import TemplateListPage from 'containers/pages/TemplateListPage';
+import TemplateEditPage from 'containers/pages/TemplateEditPage';
+
+import NotFoundPage from 'containers/pages/NotFoundPage';
+
+
+export const configureRoutes = ({ store }) => { // eslint-disable-line
+  return (
+    <Route component={App}>
+      <Route component={Main}>
+        <Route path="/">
+          <Route path="templates" component={TemplateListPage} />
+          <Route path="templates/:templateId" component={TemplateEditPage} />
+          <Route path="*" component={NotFoundPage} />
+        </Route>
+      </Route>
+    </Route>
+  );
+};
