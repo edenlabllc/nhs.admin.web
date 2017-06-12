ce5cda020638f651d311327a9ffd05544ca70f2e
Satanpit <alex.hyrenko@gmail.com>
2017-06-12 03:40:44 +0300

    Integration clinics, add filter and paging for clinics

--- app/common/redux/clinics.js
+++ app/common/redux/clinics.js
@@ -1,26 +1,27 @@
 import { handleAction, combineActions } from 'redux-actions';
-import { MOCK_API_URL } from 'config';
+import { API_URL } from 'config';
 import { normalize } from 'normalizr';
 import { createUrl } from 'helpers/url';
 import { clinic } from 'schemas';
 import { invoke } from './api';
 
 export const fetchClinics = options => invoke({
-  endpoint: createUrl(`${MOCK_API_URL}/api/legal_entities`, options),
+  endpoint: createUrl(`${API_URL}/api/legal_entities`, options),
   method: 'GET',
   headers: {
     'content-type': 'application/json',
   },
   types: ['clinics/FETCH_LIST_REQUEST', {
     type: 'clinics/FETCH_LIST_SUCCESS',
-    payload: (action, state, res) => res.json().then(
+    payload: (action, state, res) => res.clone().json().then(
       json => normalize(json.data, [clinic])
     ),
+    meta: (action, state, res) => res.clone().json().then(json => json.paging),
   }, 'clinics/FETCH_LIST_FAILURE'],
 });
 
 export const fetchClinic = id => invoke({
-  endpoint: createUrl(`${MOCK_API_URL}/api/legal_entities/${id}`),
+  endpoint: createUrl(`${API_URL}/api/legal_entities/${id}`),
   method: 'GET',
   headers: {
     'content-type': 'application/json',
@@ -43,6 +44,7 @@ export default handleAction(
   (state, action) => ({
     ...state,
     ...action.payload.entities.clinics,
+    ...action.meta,
   }),
   {}
 );
