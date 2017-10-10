24af742af1c765635ae1c0897deb6c2adac95b12
Dmitry Ivchenko <dima@nebo15.com>
2017-10-10 17:47:55 +0300

    new date (#58)

---
+++ app/common/components/reduxForm/FieldDate.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Input from 'components/Input';
+
+import FieldInput from './FieldInput';
+
+export default props => <FieldInput component={Input} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" {...props} />;
