412af511b9e8b5bed43d763bbad527ee2a10aec2
ynotleft <ynotleft@gmail.com>
2017-12-11 15:00:35 +0200

    set 'secure' flag for auth cookie

--- app/common/redux/session.js
+++ app/common/redux/session.js
@@ -4,31 +4,40 @@ import { AUTH_COOKIE_NAME } from 'config';
 
 export const getToken = () => (dispatch, getState, { cookies }) =>
   cookies.get(AUTH_COOKIE_NAME, { path: '/' });
-export const setToken = token => (dispatch, getState, { cookies }) =>
-  cookies.set(AUTH_COOKIE_NAME, token, { path: '/' });
+export const setToken = token => (dispatch, getState, { cookies }) => {
+  const cookieOption = { path: '/' };
+
+  if (process.env.NODE_ENV !== 'development') {
+    cookieOption.secure = true;
+  }
+  return cookies.set(AUTH_COOKIE_NAME, token, cookieOption);
+};
 export const removeToken = () => (dispatch, getState, { cookies }) =>
   cookies.remove(AUTH_COOKIE_NAME, { path: '/' });
 
-export const isLoginned = () => dispatch => dispatch(getToken()).then(resp => !!resp);
+export const isLoginned = () => dispatch =>
+  dispatch(getToken()).then(resp => !!resp);
 
 export const logoutAction = createAction('session/LOGOUT');
 export const setData = createAction('session/SET_DATA');
 
 export const loadTokenFromStorage = () => (dispatch, getState, { cookies }) =>
-  dispatch(setData({
-    token: cookies.get(AUTH_COOKIE_NAME, { path: '/' }),
-  }));
+  dispatch(
+    setData({
+      token: cookies.get(AUTH_COOKIE_NAME, { path: '/' })
+    })
+  );
 
 export const logout = () => dispatch =>
   dispatch(removeToken()).then(() => dispatch(logoutAction()));
 
 export const login = token => dispatch =>
-  dispatch([
-    setToken(token),
-    setData({ token }),
-  ]);
+  dispatch([setToken(token), setData({ token })]);
 
-export default handleActions({
-  [setData]: (state, action) => action.payload,
-  [logoutAction]: () => ({}),
-}, {});
+export default handleActions(
+  {
+    [setData]: (state, action) => action.payload,
+    [logoutAction]: () => ({})
+  },
+  {}
+);
